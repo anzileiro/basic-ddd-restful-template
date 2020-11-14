bbb0df9bdf89dd78de5f89a1f19422e9fc286c6f
anzileiro <anderson.anzileiro@gmail.com>
2020-11-14 15:51:03 -0300

    refactor: add controllers path to the configuration

--- src/infrastructure/rest/Configuration.ts
+++ src/infrastructure/rest/Configuration.ts
@@ -2,4 +2,6 @@ import Environment from 'env-var';
 
 export default class Configuration {
     public static HTTP_PORT: Number = Environment.get('HTTP_PORT').required(true).asInt();
+
+    public static CONTROLLER_PATH: string = '../../application/rest/v1/controller/';
 }
--- src/infrastructure/rest/Controller.ts
+++ src/infrastructure/rest/Controller.ts
@@ -1,18 +1,17 @@
 import FileSystem from 'fs';
 import Path from 'path';
+import Configuration from '@infrastructure/rest/Configuration';
 
 export default class Controller {
-    private CONTROLLER_PATH: string = '../../application/rest/v1/controllers/';
+  public async setup(): Promise<any[]> {
+    const directory: string[] = await FileSystem.readdirSync(
+      Path.join(__dirname, Configuration.CONTROLLER_PATH),
+    );
 
-    public async setup(): Promise<any[]> {
-      const directory: string[] = await FileSystem.readdirSync(
-        Path.join(__dirname, this.CONTROLLER_PATH),
-      );
+    const files: any[] = directory.map((file: string) => require(
+      Path.join(__dirname, `${Configuration.CONTROLLER_PATH}${file}`),
+    ));
 
-      const files: any[] = directory.map((file: string) => require(
-        Path.join(__dirname, `${this.CONTROLLER_PATH}${file}`),
-      ));
-
-      return files.map((instance: any) => new instance.default().setup());
-    }
+    return files.map((instance: any) => new instance.default().setup());
+  }
 }
